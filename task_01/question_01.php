dbb569c8a147f073523d008e8b9267c1dd38ae74
Asanka Srilal <asanka.srilal@gmail.com>
2012-12-22 15:59:47 +0800

    Question 01 finiehsed

--- task_01/question_01.php
+++ task_01/question_01.php
@@ -11,16 +11,38 @@
  */
 
 $inputArr = array(12, 13, 14, 15, 16);
-getGeneratedStr($inputArr);
+echo getGeneratedStr($inputArr);
 
+/**
+ * Generated the string according to the Task 01 - Question 01
+ * Assumption: All the input array elements are positive elements.
+ *
+ * @param array $inputArr positive integer set
+ * @return string $outputStr Generated string 
+ */
 function getGeneratedStr($inputArr) {
 	
 	$outputStr = '';
 	foreach($inputArr as $val) {
-		echo "$val ";
+		$outputStr.= getFizzBuzz($val) . ' '; // get the generated value (Fizz, Buzz or the original value)
 	}
 	
-	$outputStr = trim($outputStr);
+	$outputStr = trim($outputStr); // trim the string
 	return  $outputStr;
+}
+
+/**
+ * Get the generated value for a given value
+ * Used in: getGeneratedStr()
+ * Assumption: Given value is an positive integer.
+ *
+ * @param integer $value
+ * @return string/integer
+ */
+function getFizzBuzz($value) {
+	
+	$modThree = $value%3;
+	$modFive = $value%5;
+	return (($modThree == 0 || $modFive == 0) ? (($modThree == 0 ? 'Fizz' : '') . ($modFive == 0 ? 'Buzz' : '') ) : $value);
 }
 ?>
